c1c43347d951ae274d68703c7f15518e85be431d
Denis Varlakov <dsurv@yandex.ru>
2021-03-03 14:53:22 +0700

    Register new device

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use anyhow::{anyhow, bail, ensure, Context, Result};
 use derivative::Derivative;
 use futures::{SinkExt, StreamExt};
 use prost::Message as _;
+use serde::{Deserialize, Serialize};
 
 use actix_http::client::Connector;
 use awc::ws::{Frame, Message};
@@ -14,12 +15,14 @@ pub mod proto {
 
 use block_modes::BlockMode;
 use proto::web_socket_message::Type;
+use rand::rngs::OsRng;
+use rand::Rng;
 use std::time::Duration;
 
 #[actix_rt::main]
 async fn main() -> Result<()> {
     // Generate provision cipher
-    let provision_cipher = libsignal_protocol::KeyPair::generate(&mut rand::rngs::OsRng);
+    let provision_cipher = libsignal_protocol::KeyPair::generate(&mut OsRng);
 
     // Connect to provisioning API
     let mut root_certs = rustls::RootCertStore::empty();
@@ -151,6 +154,15 @@ async fn main() -> Result<()> {
 
     println!("Received provision: {:#?}", provision);
 
+    let device = create_device(
+        &mut OsRng,
+        &provision,
+        "Transport-over-signal device".into(),
+    )
+    .await?;
+
+    println!("Created device: {:#?}", device);
+
     Ok(())
 }
 
@@ -232,5 +244,88 @@ fn decrypt_provision_message(
         read_receipts: msg.read_receipts.unwrap_or_default(),
         profile_key: msg.profile_key,
         uuid: msg.uuid.unwrap_or_default(),
+    })
+}
+
+#[derive(Debug)]
+struct Device {
+    pub username: String,
+    pub device_id: u64,
+    pub password_64: String,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct CreateDeviceRequest {
+    pub name: String,
+    pub fetches_messages: bool,
+    pub registration_id: u16,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct CreateDeviceResponse {
+    pub device_id: u64,
+}
+
+async fn create_device<R: rand::Rng + rand::CryptoRng>(
+    rnd: &mut R,
+    provision: &DecryptedProvision,
+    device_name: String,
+) -> Result<Device> {
+    use awc::http::StatusCode;
+
+    let mut password = [0u8; 16];
+    rnd.fill_bytes(&mut password);
+    let mut password_64 = base64::encode(password);
+    password_64.drain(password_64.len() - 2..);
+
+    let registration_id = rnd.gen::<u16>() & 0x3fff;
+
+    let request_body = CreateDeviceRequest {
+        name: device_name,
+        fetches_messages: false,
+        registration_id,
+    };
+
+    let mut root_certs = rustls::RootCertStore::empty();
+    root_certs
+        .add_pem_file(&mut &include_bytes!("../signal-server.pem")[..])
+        .map_err(|()| anyhow!("read root ca"))?;
+
+    let mut tls_config = rustls::ClientConfig::new();
+    tls_config.root_store = root_certs;
+
+    let mut response = awc::Client::builder()
+        .connector(
+            Connector::new()
+                .rustls(tls_config.into())
+                .timeout(Duration::from_secs(30))
+                .finish(),
+        )
+        .disable_timeout()
+        .finish()
+        .put(format!(
+            "https://textsecure-service.whispersystems.org/v1/devices/{}",
+            provision.provisioning_code
+        ))
+        .basic_auth(&provision.number, Some(&password_64))
+        .send_json(&request_body)
+        .await
+        .map_err(|e| anyhow!("creating new device: {}", e))?;
+
+    ensure!(
+        response.status() == StatusCode::OK,
+        "creating new device: server returned {}",
+        response.status()
+    );
+
+    let created_device: CreateDeviceResponse =
+        response.json().await.context("parse server response")?;
+
+    Ok(Device {
+        username: format!("{}.{}", provision.number, created_device.device_id),
+        device_id: created_device.device_id,
+        password_64,
     })
 }
